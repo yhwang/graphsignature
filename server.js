6010d974b1d4092ce9c153d6a4d4b0717701e468
Yihong Wang <yh.wang@ibm.com>
2017-07-31 15:59:06 -0700

    Minor changes
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var express = require("express");
 var app = express();
 var cfenv = require("cfenv");
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.render('signature', { title: 'Yihong Wang' });
 });
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+  console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
